24fb9d51cefa0d4385b084eee17cb7c8d72a5ec4
Dahye Kim <dahye.kim@nhnnext.org>
2015-07-08 17:28:07 +0900

    rod cutting bottom up, Price class deleted

--- week1_dynamic/src/rodCutting/RodCutting.java
+++ week1_dynamic/src/rodCutting/RodCutting.java
@@ -1,25 +1,23 @@
 package rodCutting;
 
-import java.util.ArrayList;
 
 public class RodCutting {
-	public static int rodCutting (int n, ArrayList<Price> prices) {
+	public static int rodCutting (int n, int[] prices) {
 		if(n < 0) new IllegalArgumentException("n shold be nonnegative");
-		if(n < 2) return prices.get(n).price; //더이상 잘라질 수 없는 것 recursion의 base step
+		if(n < 2) return prices[n]; //더이상 잘라질 수 없는 것 recursion의 base step
 		
 		int maxIdx = 0;
 		int[] maxArray = new int[n+1];
-		maxArray[0] = prices.get(n).price;
+		maxArray[0] = prices[n];
 		for(int i = 1; i<=n; i++) {
-			maxArray[i] = prices.get(i).price + rodCutting(n-i, prices);
+			maxArray[i] = prices[i] + rodCutting(n-i, prices);
 			if(maxArray[maxIdx] < maxArray[i]) maxIdx = i;
 		}
 		
-		prices.get(maxIdx).cutted = true;
 		return maxArray[maxIdx];
 	}
 	
-	public static int rodCuttingTopDown(int n, ArrayList<Price> prices) {
+	public static int rodCuttingTopDown(int n, int[] prices) {
 		int[] rod = new int[n+1];
 		for(int i = 0; i < rod.length; i++) {
 			 rod[i] = -1;
@@ -27,25 +25,41 @@ public class RodCutting {
 		return rodCuttingTopDownAux(n, prices, rod);
 	}
 	
-	private static int rodCuttingTopDownAux (int n, ArrayList<Price> prices, int[] rod) {
+	private static int rodCuttingTopDownAux (int n, int[] prices, int[] rod) {
 		if(n < 0) new IllegalArgumentException("n shold be nonnegative");
 		
 		if(rod[n] >= 0) return rod[n]; //memoization
 
 		if(n < 2) {
-			rod[n] = prices.get(n).price; //memo
+			rod[n] = prices[n]; //memo
 		}
 		
 		int maxIdx = 0;
 		int[] maxArray = new int[n+1];
-		maxArray[0] = prices.get(n).price;
+		maxArray[0] = prices[n];
 		for(int i = 1; i<=n; i++) {
-			maxArray[i] = prices.get(i).price + rodCuttingTopDownAux(n-i, prices, rod);
+			maxArray[i] = prices[i] + rodCuttingTopDownAux(n-i, prices, rod);
 			if(maxArray[maxIdx] < maxArray[i]) maxIdx = i;
 		}
 		
-		prices.get(maxIdx).cutted = true;
 		rod[n] = maxArray[maxIdx]; //memo
+		return rod[n];
+	}
+	
+	public static int rodCuttingBottomUp(int n, int[] prices) {
+		if(n < 0) new IllegalArgumentException("n shold be nonnegative");
+		int [] rod = new int[n+1];
+		rod[0] = prices[0];
+		rod[1] = prices[1];
+		
+		for(int i = 2; i <= n; i++) {
+			rod[i] = prices[i];
+			for(int j = 1; j <= i/2; j++) {
+				if(rod[i] < prices[j] + rod[i-j]) {
+					rod[i] = prices[j] + rod[i-j]; // rod[j] + rod[i-j]로 대체할 수 있
+				}
+			}
+		}
 		return rod[n];
 	}
 }
--- week1_dynamic/test/rodCutting/RodCunttingTest.java
+++ week1_dynamic/test/rodCutting/RodCunttingTest.java
@@ -1,38 +1,38 @@
 package rodCutting;
 
-import static org.junit.Assert.*;
-import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class RodCunttingTest {
-	ArrayList<Price> prices = new ArrayList<Price>();
+	int[] prices = new int[11];
 
 	@Before
 	public void setup() {
-		Price p0 = new Price(0);
-		Price p1 = new Price(1);
-		Price p2 = new Price(5);		
-		Price p3 = new Price(8);
-		Price p4 = new Price(9);
-		Price p5 = new Price(10);
-		Price p6 = new Price(17);
-		Price p7 = new Price(17);
-		Price p8 = new Price(20);
-		Price p9 = new Price(24);
-		Price p10 = new Price(30);
+		int p0 = 0;
+		int p1 = 1;
+		int p2 = 5;		
+		int p3 = 8;
+		int p4 = 9;
+		int p5 = 10;
+		int p6 = 17;
+		int p7 = 17;
+		int p8 = 20;
+		int p9 = 24;
+		int p10 = 30;
 		
-		prices.add(0, p0);
-		prices.add(1, p1);
-		prices.add(2, p2);
-		prices.add(3, p3);
-		prices.add(4, p4);
-		prices.add(5, p5);
-		prices.add(6, p6);
-		prices.add(7, p7);
-		prices.add(8, p8);
-		prices.add(9, p9);
-		prices.add(10, p10);
+		prices[0] = p0;
+		prices[1] = p1;
+		prices[2] = p2;
+		prices[3] = p3;
+		prices[4] = p4;
+		prices[5] = p5;
+		prices[6] = p6;
+		prices[7] = p7;
+		prices[8] = p8;
+		prices[9] = p9;
+		prices[10] = p10;
 	}
 	
 	@Test
@@ -45,6 +45,12 @@ public class RodCunttingTest {
 	public void testTopDown() {
 		assertEquals(RodCutting.rodCuttingTopDown(4, prices), 10);
 		assertEquals(RodCutting.rodCuttingTopDown(5, prices), 13);
+	}
+	
+	@Test
+	public void testBottomUp() {
+		assertEquals(RodCutting.rodCuttingBottomUp(4, prices), 10);
+		assertEquals(RodCutting.rodCuttingBottomUp(5, prices), 13);
 	}
 
 }
