0b0a68df4c4c84bf85e334c443b4a5b3afaca4b0
Dahye Kim <dahye.kim@nhnnext.org>
2015-09-24 00:29:58 +0900

    #13 naive string matcher

--- stringMatcher/src/stringMatcher/StringMatcher.java
+++ stringMatcher/src/stringMatcher/StringMatcher.java
@@ -8,33 +8,63 @@ public class StringMatcher {
 		String pattern1 = "ababc";
 		String pattern2 = "";
 		String pattern3 = "a";
-		
+
 		KMPmatcher(text1, pattern1);
-		System.out.println("h");
 		KMPmatcher(text2, pattern1);
 		KMPmatcher(text2, pattern2);
 		KMPmatcher(text2, pattern3);
+		
+		naiveMatcher(text1, pattern1);
+		naiveMatcher(text2, pattern1);
+		naiveMatcher(text2, pattern2);
+		naiveMatcher(text2, pattern3);
 	}
-	
-	static void KMPmatcher(String text, String pattern) {
+
+	static void naiveMatcher(String text, String pattern) {
+		System.out.println("naive text: " + text);
+		System.out.println("naive pattern: " + pattern);
+		
 		int n = text.length();
 		int m = pattern.length();
-		if(m <= 0) 
-			return;
 		
+		if (m <= 0)
+			return;
+
+		for (int textPos = 0; textPos < n - m + 1; textPos++) {
+			if(text.substring(textPos, textPos + m).equals(pattern)){
+				System.out.println("match! pos: " + textPos);
+				System.out.println("substring: " + text.substring(textPos, textPos + m));
+			}
+		}
+	}
+
+	static void KMPmatcher(String text, String pattern) {
+		System.out.println("KMP text: " + text);
+		System.out.println("KMP pattern: " + pattern);
+
+		int n = text.length();
+		int m = pattern.length();
+		if (m <= 0)
+			return;
+
 		int[] prefixFunction = computePrefixFunction(pattern);
-		int q = 0; // // maxPrefixFunctionValueUntilPreviousPos
+		/*
+		 * q의 의미.
+		 * 이전 위치까지 봤을 때, prefix 일치하는 것 중 가장 큰 개수. 
+		 * 하지만 index로 쓸 때는 -1 해줘야 한다. (*) 참고.
+		 * */
+		int q = 0; 
 		for (int textPos = 0; textPos < n; textPos++) {
 			while (q > 0 && text.charAt(textPos) != pattern.charAt(q)) {
-				q = prefixFunction[q - 1];
+				q = prefixFunction[q - 1]; // (*)이렇게.
 			}
 			if (text.charAt(textPos) == pattern.charAt(q)) {
 				q++;
 			}
 			if (q == m) {
+				q = prefixFunction[q - 1];
 				System.out.println("match! pos: " + (textPos - m + 1));
 				System.out.println("substring: " + text.substring(textPos - m + 1, textPos + 1));
-				q = prefixFunction[q-1];
 			}
 		}
 	}
@@ -43,7 +73,12 @@ public class StringMatcher {
 		int m = pattern.length();
 		int[] result = new int[m];
 		result[0] = 0; // trivial
-		int q = 0; // maxPrefixFunctionValueUntilPreviousPos
+		/*
+		 * q의 의미.
+		 * 이전 위치까지 봤을 때, prefix 일치하는 것 중 가장 큰 개수. 
+		 * 하지만 index로 쓸 때는 -1 해줘야 한다. (*) 참고.
+		 * */
+		int q = 0; 
 		for (int pos = 1; pos < m; pos++) {
 			while (q > 0 && pattern.charAt(pos) != pattern.charAt(q)) {
 				q = result[q - 1];
@@ -52,9 +87,7 @@ public class StringMatcher {
 				q++;
 			}
 			result[pos] = q;
-			System.out.println(q);
 		}
-		System.out.println("compute completed");
 		return result;
 	}
 }
